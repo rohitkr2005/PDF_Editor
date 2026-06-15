9cfa39371065ab0bdebf606d1f509632f4a961f6
Balram Mandal <rkrm0209@gmail.com>
2026-06-15 19:21:12 +0530

    solved deployment error in app.py

--- app.py
+++ app.py
@@ -524,5 +524,7 @@ def pdf_info():
 
 
 # ─────────────────────────────────────────────────────────────────────────────
+
 if __name__ == "__main__":
-    app.run(debug=True, use_reloader=False, port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
